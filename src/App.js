afe4f4be99d925284309e348302906ee6e1e8c38
Palak Gupta <palakgupta2712@gmail.com>
2021-04-03 21:47:48 +0530

    semantic-ui added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./styles.css";
-
+import "semantic-ui-css/semantic.min.css";
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <div>React Google Sheets</div>;
 }
