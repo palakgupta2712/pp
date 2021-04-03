431eb6b429404e85b9caf20eccd2dbac61afb24f
Palak Gupta <palakgupta2712@gmail.com>
2021-04-03 21:52:50 +0530

    Form UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import "./styles.css";
 import "semantic-ui-css/semantic.min.css";
+import { Button, Header, Container, Form } from "semantic-ui-react";
 export default function App() {
-  return <div>React Google Sheets</div>;
+  return (
+    <Container fluid className="container">
+      <Header as="h1"> React Google Sheets </Header>
+      <Form>
+        <Form.Field>
+          <label>Name</label>
+          <input placeholder="Enter Your Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Age</label>
+          <input placeholder="Enter Your Age" />
+        </Form.Field>
+        <Form.Field>
+          <label>Salary</label>
+          <input placeholder="Enter Your Salary" />
+        </Form.Field>
+        <Form.Field>
+          <label>Hobby</label>
+          <input placeholder="Enter Your Hobby" />
+        </Form.Field>
+
+        <Button color="black" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 }
